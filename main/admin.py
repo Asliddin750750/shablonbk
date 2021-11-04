80abf0d21f95cf044aff97d760d23994ca02ef55
asliddin7501 <mahmudovasliddin750@gmail.com>
2021-11-04 11:43:16 +0500

    admin Country

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Country
+
+
+class CountryAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+admin.site.register(Country, CountryAdmin)
